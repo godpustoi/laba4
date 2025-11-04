970e9302064d7f554ac2bad3a361a184ed1aa17c
Asanov Arseny <asanov.aa0009@gmail.com>
2025-11-04 08:48:45 +0300

    Добавил файл a2.py

--- a2.py
+++ a2.py
@@ -1,30 +1,29 @@
-import string
+rows = int(input("Введите rows: "))
+columns = int(input("Введите columns: "))
+ch = input("Введите ch: ")
 
-password = input("Введите пароль: ")
+def draw_rectangle(rows, columns, ch):
+    for i in range(rows):
+        for j in range(columns):
+            print(ch, end=" ")
+        print()
 
-errors = []
+def draw_triangle(rows, ch):
+    for i in range(rows +1):
+        for j in range(i):
+            print(ch, end=" ")
+        print()
 
-if len(password) != 8:
-    errors.append("Длина пароля не равна 8")
 
-if password.lower() == password:
-    errors.append("В пароле отсутствуют заглавные буквы")
+def draw_frame(rows, columns, ch):
+    for i in range(rows):
+        for j in range(columns):
+            if i == 0 or i == rows - 1  or j == 0 or j == columns - 1:
+                print(ch, end=" ")
+            else:
+                print(" ", end=" ")
+        print()
 
-if password.upper() == password:
-    errors.append("В пароле отсутствуют строчные буквы")
-
-if not any(symbol.isdigit() for symbol in password):
-    errors.append("В пароле отсутствуют цифры")
-
-if not any(symbol in "*-#" for symbol in password):
-    errors.append("В пароле отсутствуют специальные символы")
-
-allowed = string.ascii_uppercase + string.ascii_lowercase + string.digits + "*-#"
-if not all(symbol in allowed for symbol in password):
-    errors.append("В пароле используются непредусмотренные символы")
-
-if len(errors) == 0:
-    print("Надежный пароль")
-else:
-    for error in errors:
-        print(error)
+draw_rectangle(rows, columns, ch)
+draw_triangle(rows, ch)
+draw_frame(rows, columns, ch)
